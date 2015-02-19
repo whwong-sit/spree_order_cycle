fad9a6d8fd982b00d7e74243f4216a0055a51ac1
whwong <whwong@it.usyd.edu.au>
2015-02-19 23:48:55 +1100

    Basic Order Cycle page works

--- app/controllers/order_cycles_controller.rb
+++ app/controllers/order_cycles_controller.rb
@@ -2,11 +2,36 @@ module Spree
     module Admin
         class OrderCyclesController < ResourceController
 
-            def index
-                session[:return_to] = request.url
-                @order_cycles = OrderCycle.all
+        before_action :check_json_authenticity, only: :index
+        before_action :load_roles
+
+        def index
+            #session[:return_to] = request.url
+            #params[:q] ||= {}
+            respond_with(@collection) do |format|
+                format.html
+                format.json { render :json => json_data }
             end
+        end
+
+        def model_class
+            Spree::OrderCycle
+        end
+
+        protected 
+
+            def collection
+                return @collection if @collection.present?
+
+                #@search = Spree::OrderCycle.accessible_by(current_ability, :index).ransack(params[:q])
+                @search = Spree::OrderCycle.ransack(params[:q])
+                @collection = @search.result.page(params[:page]).per(params[:per_page] || Spree::Config[:orders_per_page])
+            end
+
+            def load_roles
+                @roles = Spree::Role.all
+            end
+            
         end
     end
 end
-
