a9b790281ee53d14b81e5663443317b350e18ab4
whwong <whwong@it.usyd.edu.au>
2015-02-20 12:40:46 +1100

    Order Cycle linked with Orders predicated on latters' complete times

--- app/controllers/order_cycles_controller.rb
+++ app/controllers/order_cycles_controller.rb
@@ -6,12 +6,69 @@ module Spree
         before_action :load_roles
 
         def index
-            #session[:return_to] = request.url
-            #params[:q] ||= {}
             respond_with(@collection) do |format|
                 format.html
                 format.json { render :json => json_data }
             end
+        end
+
+        def edit
+            @order_cycle = Spree::OrderCycle.find(params[:id])
+            @cy_orders = @order_cycle.orders
+            #@orders = Spree::Order.all
+            cycle_start = @order_cycle.start
+            cycle_end = @order_cycle.end
+            @pot_orders = Spree::Order.where(:state => 'complete')
+                                      .where("completed_at >= ?", cycle_start)
+                                      .where("completed_at < ?", cycle_end)
+
+            @list_items = @order_cycle.line_items
+            @ord_search = Spree::Order.ransack(params[:q])
+        end
+
+        def create
+
+            #find orders within the order_cycle time range and add them to the cycle
+            # if they do not have an assigned order_cycle_id
+            #debug(params)
+            avail_orders = Spree::Order.where(:state => 'complete')
+            .where(order_cycle_id: [false, nil])
+            .where("completed_at >= ?", params[:order_cycle][:start])
+            .where("completed_at < ?", params[:order_cycle][:end])
+            @order_cycle.orders << avail_orders
+
+            super
+            #invoke_callbacks(:create, :before)
+            #@object.attributes = permitted_resource_params
+
+            #if @object.save
+                #invoke_callbacks(:create, :after)
+
+
+                #flash[:success] = flash_message_for(@object, :successfully_created)
+                #respond_with(@object) do |format|
+                    #format.html { redirect_to location_after_save }
+                    #format.js   { render :layout => false }
+                #end
+            #else
+                #invoke_callbacks(:create, :fails)
+                #respond_with(@object) do |format|
+                    #format.html do
+                        #flash.now[:error] = @object.errors.full_messages.join(", ")
+                        #render action: 'new'
+                    #end
+                    #format.js { render layout: false }
+                #end
+            #end
+        end
+
+        def destroy
+            order_cycle = Spree::OrderCycle.find(params[:id])
+            order_cycle.orders.each {|order| 
+                order.order_cycle_id = nil
+                order.save
+            }
+            super
         end
 
         def model_class
--- app/models/spree/order_cycle.rb
+++ app/models/spree/order_cycle.rb
@@ -1,5 +1,19 @@
 module Spree
     class OrderCycle < Spree::Base
         has_many :orders
+        has_many :line_items, :through => :orders
+            # This grouping and selecting should be done in either the controller or view
+            # see http://stackoverflow.com/questions/21531055/rails-has-many-through-grouping-and-summing-data for e.g.
+            #-> { group(:variant_id).select(
+            #:group => "variant_id, price, currency, cost_price",
+            #:select
+                #"spree_line_items.variant_id", 
+                #"spree_line_items.price", 
+                #"spree_line_items.currency", 
+                #"spree_line_items.cost_price", 
+                #"SUM(spree_line_items.quantity) AS quantity")
+                ##I have omitted tax & promo related fields as they  
+                ## are specific to individual orders
+            #},        
     end
 end
